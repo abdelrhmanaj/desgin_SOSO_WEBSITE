287ec72a27fdcada0bded02c7460e5dd07478867
Abdelrhmanaj <abdelrhmanaja@gmail.com>
2026-05-30 02:52:07 +0300

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['designsososwebsite-production.up.railway.app'],
+    allowedHosts: true,
   },
 })
-
